cef5deefa9dd2e6a4b46978e689682b5f9a65e53
Alex <alexartnn@mail.ru>
2019-08-15 13:20:34 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,28 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
+
+
+
+/*let answer = confirm("Are you here");
+console.log(answer);*/
+
+let answer = +prompt("тебе 18?", "Good");
+console.log(typeof(answer));
+console.log("arr" + " - object");
+console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log()
